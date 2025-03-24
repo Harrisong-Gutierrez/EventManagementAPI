b85405932b2d16dca466962490bd7d3b07d07e5d
Harrisong <harrygutie7@gmail.com>
2025-03-24 11:43:14 -0600

    fixing warnings

--- DTOs/EventDto.cs
+++ DTOs/EventDto.cs
@@ -3,9 +3,9 @@
     public class EventDto
     {
         public Guid EventId { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public DateTime Date { get; set; }
-        public string? Location { get; set; }
-        public string? Description { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
     }
 }
--- DTOs/SponsorDto.cs
+++ DTOs/SponsorDto.cs
@@ -3,7 +3,7 @@
     public class SponsorDto
     {
         public Guid SponsorId { get; set; }
-        public string? Name { get; set; }
-        public string? ContactInfo { get; set; }
+        public string Name { get; set; }
+        public string ContactInfo { get; set; }
     }
 }
